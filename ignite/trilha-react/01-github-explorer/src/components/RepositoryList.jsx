c48f87dde2ddbae1696b5c281055d7e0a7cda22d
Renan Fermino <renanfermino.jc@outlook.com>
2021-04-03 20:20:04 -0300

    primeiro component react

---
+++ ignite/trilha-react/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,50 @@
+export function RepositoryList() {
+  return (
+    <section className="repository-list">
+      <h1>Lista de repositorórios</h1>
+
+      <ul>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            acessar repositório
+          </a>
+        </li>
+        <li>
+          <strong>unform</strong>
+          <p>Forms in React</p>
+
+          <a href="">
+            acessar repositório
+          </a>
+        </li>
+      </ul>
+    </section>
+  )
+}
