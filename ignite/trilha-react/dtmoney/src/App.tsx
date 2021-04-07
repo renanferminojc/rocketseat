9566d9642899345f491813e1c08b9d93604d102b
Renan Fermino <renanfermino.jc@outlook.com>
2021-04-06 22:25:27 -0300

    componente newTransactionModal

--- ignite/trilha-react/dtmoney/src/App.tsx
+++ ignite/trilha-react/dtmoney/src/App.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import Modal from 'react-modal'
 
 import { Dashboard } from "./components/Dashboard";
 import {Header} from "./components/Header";
+import { NewTransactionModal } from './components/NewTransactionModal';
 import { GlobalStyle } from "./styles/global";
 
 Modal.setAppElement('#root');
@@ -10,22 +11,23 @@ Modal.setAppElement('#root');
 export function App() {
   const [isNewTransactionModalOpen, setIsNewTransactionModalOpen] = useState(false);
 
-  function handleOpenNewTransactionModal() {
-    setIsNewTransactionModalOpen(true)
-  }
   function handleCloseNewTransactionModal() {
     setIsNewTransactionModalOpen(false)
+  }
+
+  function handleOpenNewTransactionModal() {
+    setIsNewTransactionModalOpen(true)
   }
   return (
     <>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
-      <Modal 
-          isOpen={isNewTransactionModalOpen}
-          onRequestClose={handleCloseNewTransactionModal}
-        >
-          <h2>Cadastrar transação</h2>
-      </Modal>
+      
+      <NewTransactionModal  
+        isOpen={isNewTransactionModalOpen}
+        onRequestClose={handleCloseNewTransactionModal}
+      />
+
       <GlobalStyle />
     </>
   );
