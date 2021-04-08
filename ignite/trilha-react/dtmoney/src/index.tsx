dc6a310f08654e2f4c4972f25ead93266aac236c
Renan Fermino <renanfermino.jc@outlook.com>
2021-04-08 13:54:47 -0300

    formatando valores

--- ignite/trilha-react/dtmoney/src/index.tsx
+++ ignite/trilha-react/dtmoney/src/index.tsx
@@ -8,20 +8,43 @@ createServer({
     transaction: Model,
   },
 
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: "Aplicação Web com ReactJS",
+          type: "deposit",
+          category: "jobs",
+          amount: 6000,
+          createdAt: new Date("2021-02-12 09:00:00"),
+        },
+        {
+          id: 2,
+          title: "Aluguel",
+          type: "withdraw",
+          category: "house",
+          amount: 800,
+          createdAt: new Date("2021-03-01 10:00:00"),
+        },
+      ],
+    });
+  },
+
   routes() {
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return this.schema.all('transaction')
-    })
+      return this.schema.all('transaction');
+    });
 
-    this.get('/transactions', (schema, request) => {
-      const data = JSON.parse(request.requestBody)
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody);
 
       return schema.create('transaction', data);
-    })
-  }
-})
+    });
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>
