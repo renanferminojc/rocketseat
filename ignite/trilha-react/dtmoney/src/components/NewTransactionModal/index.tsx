f361adec9aeb4cf505618f18309c181371bf09fa
Renan Fermino <renanfermino.jc@outlook.com>
2021-04-08 11:03:15 -0300

    cores dos botoes

--- ignite/trilha-react/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ignite/trilha-react/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -1,8 +1,9 @@
 import Modal from 'react-modal'
-import { Container, TransactionTypeContainer } from './styles'
+import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
+import { useState } from 'react'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,6 +11,8 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [type, setType] = useState('deposit');
+
   return (
     <Modal 
       isOpen={isOpen}
@@ -38,15 +41,25 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         />
 
         <TransactionTypeContainer>
-          <button type="button">
-            <img src={incomeImg} alt="income image"/>
+          <RadioBox 
+            type="button" 
+            onClick={() => { setType('deposit')}}
+            isActive={type === 'deposit'}
+            activeColor="green"
+          >
+            <img src={incomeImg} alt="income"/>
             <span>Entrada</span>
-          </button>
+          </RadioBox>
 
-          <button type="button">
-            <img src={outcomeImg} alt="outcome image"/>
+          <RadioBox 
+            type="button"
+            onClick={() => { setType('withdraw')}}
+            isActive={type === 'withdraw'}
+            activeColor="red"
+          >
+            <img src={outcomeImg} alt="outcome"/>
             <span>Saída</span>
-          </button>
+          </RadioBox>
         </TransactionTypeContainer>
 
         <input
