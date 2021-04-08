e316cb7edda94c6d7f408f35a0589c55eaa9dff6
Renan Fermino <renanfermino.jc@outlook.com>
2021-04-08 11:51:38 -0300

    inserindo transacoes na api

--- ignite/trilha-react/dtmoney/src/components/NewTransactionModal/index.tsx
+++ ignite/trilha-react/dtmoney/src/components/NewTransactionModal/index.tsx
@@ -3,7 +3,8 @@ import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import closeImg from '../../assets/close.svg'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
-import { useState } from 'react'
+import { FormEvent, useState } from 'react'
+import { api } from '../../services/api'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -11,7 +12,24 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCraeteNewTransaction(event: FormEvent) {
+    event.preventDefault();
+
+    const data = {
+      title,
+      value,
+      category,
+      type
+    };
+
+    api.post('/transactions', data);
+    
+  }
 
   return (
     <Modal 
@@ -28,16 +46,20 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
         <img src={closeImg} alt="fechar modal"/>
       </button>
 
-      <Container>
+      <Container onSubmit={handleCraeteNewTransaction}>
         <h2>Cadastrar transação</h2>
 
         <input
           placeholder="Titulo"
+          value={title}
+          onChange={event => setTitle(event.target.value)}
         />
 
         <input
           type="number"
           placeholder="valor"
+          value={value}
+          onChange={event => setValue(Number(event.target.value))}
         />
 
         <TransactionTypeContainer>
@@ -64,6 +86,8 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
 
         <input
           placeholder="Categoria"
+          value={category}
+          onChange={event => setCategory(event.target.value)}
         />
 
         <button type="submit">
