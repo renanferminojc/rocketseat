577b77ce5dc1e960dfb93b52aa6ce34c50a37b6e
Renan Fermino <renanfermino.jc@outlook.com>
2021-04-11 18:09:29 -0300

    static site generation (ssg)

--- ignite/trilha-react/ignews/src/pages/index.tsx
+++ ignite/trilha-react/ignews/src/pages/index.tsx
@@ -1,4 +1,4 @@
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import Head from 'next/head';
 import { stripe } from '../../services/stripe';
 import { SubscribeButton } from '../components/SubscribeButton';
@@ -34,7 +34,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1If8ODBf1GNenIDP6Lsmyu62')
 
   const product = {
@@ -48,6 +48,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
